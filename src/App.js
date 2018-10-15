d9c40bcb90094221c6e2026360e7ed1e3faeae2c
serve77r <1931130013@qq.com>
2018-10-15 23:14:27 +0800

    20181015基本布局

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dataArr from './data.json';
+// console.log(dataArr)
+
+class ImgFigure extends Component {
+  render(){
+    return (
+        <figure className="img-figure">
+          <img src={this.props.data.img} />
+          <figcaption>
+            <h2 className="img-title">{this.props.data.tit}</h2>
+          </figcaption>
+        </figure>
+      )
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section className="stage">
+        <section className="img-sec">
+          {
+            dataArr.map((item,index)=>{
+              return (<ImgFigure data={item} key={index} />)
+            })
+          }
+        </section>
+        <nav className="controller-nav">
+        </nav>
+      </section>
     );
   }
 }
