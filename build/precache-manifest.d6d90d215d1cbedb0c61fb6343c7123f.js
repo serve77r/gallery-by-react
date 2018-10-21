5c189024603b43de5cd1ee1a05754fd67c1254fa
serve77r <1931130013@qq.com>
2018-10-21 23:21:39 +0800

    添加build

---
+++ build/precache-manifest.d6d90d215d1cbedb0c61fb6343c7123f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "21640507b82a2a5480e3",
+    "url": "/static/js/main.21640507.chunk.js"
+  },
+  {
+    "revision": "accd7dfab4b02c7bbf7c",
+    "url": "/static/js/1.accd7dfa.chunk.js"
+  },
+  {
+    "revision": "21640507b82a2a5480e3",
+    "url": "/static/css/main.348c8e35.chunk.css"
+  },
+  {
+    "revision": "972c9b3da705bb3d7ade2a0de0e18c14",
+    "url": "/index.html"
+  }
+];
